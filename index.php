64be7e00ca62310e27be928878e67e27452325dd
Basif01 <asifbashar18@gmail.com>
2019-08-11 19:01:51 +0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,89 @@
+<html>
+	<head>
+		<title>UGC USER</title>
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+	</head>
+	<body>
+		<form action="#" method="post">
+		<div class="container">
+			<br />
+			<br />
+			<h2 align="center"><a href="http://www.webslesson.info/2016/02/dynamically-add-remove-input-fields-in-php-with-jquery-ajax.html">University Information</a></h2><br />
+			<div class="form-group">
+				<form name="add_name" id="add_name">
+					<div class="table-responsive">
+						<table class="table table-bordered" id="dynamic_field">
+						
+							<tr>
+								
+								<td><input type="text" name="name[]" placeholder="University Name" class="form-control name_list" /></td>
+								<td><button type="button" name="add" id="add" class="btn btn-success">Add More</button></td>
+							</tr>
+						</table>
+						<div>
+							<input type="checkbox" name="year" value="TotalSchool">2017</input>
+							<input type="checkbox" name="year" value="TotalSchool">2018</input>
+							<input type="checkbox" name="year" value="TotalSchool">2019</input>
+							<br>
+						</div>
+<div>
+	<input type="checkbox" name="type" value="TotalStudent">TotalStudent</input>
+<input type="checkbox" name="type" value="TotalSchool">Total School</input>
+<input type="checkbox" name="type" value="TotalStudent">TotalFaculty</input>
+<input type="checkbox" name="type" value="TotalStudent">Land Area</input>
+
+</div>
+<br>
+						<input type="button" name="submit" id="submit" class="btn btn-info" value="Submit" />
+<form action="#" method="post">
+	<table>
+		
+</table>
+</form>
+</form>
+<?php
+if (isset($_POST['type'])){
+echo $_POST['type']; // Displays value of checked checkbox.
+}
+?>
+					</div>
+				</form>
+			</div>
+		</div>
+	</body>
+</html>
+
+<script>
+$(document).ready(function(){
+	var i=1;
+	$('#add').click(function(){
+		i++;
+		$('#dynamic_field').append('<tr id="row'+i+'"><td><input type="text" name="name[]" placeholder="University Name" class="form-control name_list" /></td><td><button type="button" name="remove" id="'+i+'" class="btn btn-danger btn_remove">X</button></td></tr>');
+	});
+	
+	$(document).on('click', '.btn_remove', function(){
+		var button_id = $(this).attr("id"); 
+		$('#row'+button_id+'').remove();
+	});
+	
+	$('#submit').click(function(){		
+		$.ajax({
+			url:"name.php",
+			method:"POST",
+			data:$('#add_name').serialize(),
+			success:function(data)
+			{
+				alert(data);
+				$('#add_name')[0].reset();
+			}
+		});
+	});
+	
+});
+</script>
+
+
+
+
